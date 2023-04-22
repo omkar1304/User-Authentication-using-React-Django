fbaf9f30bd12a5a7643b6d6538243e0c493c1842
Omkar Pedamkar <omkarpedamkar2@gmail.com>
2023-04-23 03:52:16 +0530

    Update settings.py

--- djangoauthapi/djangoauthapi/settings.py
+++ djangoauthapi/djangoauthapi/settings.py
@@ -147,7 +147,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'omkarpedamkar2@gmail.com'
-EMAIL_HOST_PASSWORD = 'itmayjykzkztuxvx'
+EMAIL_HOST_PASSWORD = '*****'
 
 
 
